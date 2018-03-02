a3973c923b8780d8da64ac56e81b5d0f99cd3b5e
Jordan Anaya <jordananaya@gmail.com>
2018-03-01 19:40:56 -0500

    february analyses

--- analyses/preprint_month_graph.py
+++ analyses/preprint_month_graph.py
@@ -272,7 +272,7 @@ ax.legend(loc=2,frameon=False,fontsize=21,ncol=4)
 
 
 ##plt.savefig('chart.pdf')
-plt.savefig('january_preprints.png')
+plt.savefig('february_preprints.png')
 #pillow might be needed for the jpg
 ##plt.savefig('chart.jpg')
 plt.show()
