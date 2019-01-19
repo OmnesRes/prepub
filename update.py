bbd4fda9cf9922e790b94d8c0fad425f3abb6387
Jordan Anaya <jordananaya@gmail.com>
2019-01-19 09:00:27 -0500

    biorxiv dates

--- update.py
+++ update.py
@@ -7,8 +7,8 @@ date_dict={"January":1,"February":2,"March":3,"April":4,"May":5,"June":6,
            "Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,
            "Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
 
-reverse_dict={"1":"January","2":"February","3":"March","4":"April","5":"May","6":"June",
-           "7":"July","8":"August","9":"September","10":"October","11":"November","12":"December"}
+reverse_dict={"01":"January","02":"February","03":"March","04":"April","05":"May","06":"June",
+           "07":"July","08":"August","09":"September","10":"October","11":"November","12":"December"}
 
 def reverse_date(date):
     year=date.split('-')[0]
